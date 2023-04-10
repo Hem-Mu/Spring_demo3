77afa4e9aab1d52f72bdaedae3ba23abaf813baf
Hem-Mu <kimmim1223@gmail.com>
2023-04-10 16:55:49 +0900

    서블릿 요청 처리 샘플

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -22,9 +25,10 @@ public class HellobootApplication {
 			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					resp.setStatus(200);
-					resp.setHeader("Content-Type","text/plain");
-					resp.getWriter().println("Hello");
+					String name = req.getParameter("name");
+					resp.setStatus(HttpStatus.OK.value()); //200
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello"+name);
 				}
 			})// hello로 들어 오면 위의 클래스로 처리
 			.addMapping("/hello");
